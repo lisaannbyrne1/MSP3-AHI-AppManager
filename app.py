e6011279872542097d99ea1943151aa23b8a62b1
Lisa Byrne <lisaannbyrne1@hotmail.com>
2020-04-22 11:30:17 +0000

    link to DB & create html template pages

--- app.py
+++ app.py
@@ -1,12 +1,52 @@
 import os
-from flask import Flask
+if os.path.exists("env.py"):
+    import env
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'ClientTaskManager'
+app.config["MONGO_URI"] = 'mongodb://secret_key = os.environ.get("MONGO_URI")'
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_events')
+def get_events():
+    return render_template('events.html')
+
+
+@app.route('/add_events')
+def add_events():
+    return render_template('addevents.html')
+
+
+@app.route('/edit_events')
+def edit_events():
+    return render_template('editevents.html')
+
+
+@app.route('/get_status')
+def get_status():
+    return render_template('status.html')
+
+
+@app.route('/edit_status')
+def edit_status():
+    return render_template('updatestatus.html')
+
+
+@app.route('/get_purpose')
+def get_purpose():
+    return render_template('purpose.html')
+
+
+@app.route('/get_users')
+def get_users():
+    return render_template('users.html')
 
 
 if __name__ == '__main__':
